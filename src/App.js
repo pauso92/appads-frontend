5f3a22d49e61894d9dcbe167979d61fce15c2b9f
Edgar Tapia <edgar.tapia@outlook.es>
2020-03-20 12:19:31 -0600

    [add] se agrego el Router en la app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import logo from './logo.svg';
 // import './App.css';
+
 import Report_view from './Report_view';
 import Login from './Login'
 //import logo from './logo.svg';
@@ -9,14 +11,35 @@ import ChartsItem from './ChartsItem'
 //import './App.css'
 
 function App() {
-
   return (
     <div className="App">
+    <Router>
+      <ul>
+        <li>
+          <Link to="/Login">Login</Link>
+        </li>
+        <li>
+          <Link to="/Report_view">Crear reporte</Link>
+        </li>
+        <li>
+          <Link to="/TableItem">TableItem</Link>
+        </li>
+        <li>
+          <Link to="/ChartsItem">ChartsItem</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/Login" component={Login} />
+        <Route exact path="/Report_view" component={Report_view} />
+        <Route exact path="/TableItem" component={TableItem} />
+        <Route exact path="/ChartsItem" component={ChartsItem} />
+      </Switch>
+    </Router>
       {/* < Report_view/> */}
-      <Login />
-      <p>hola</p>
-      <TableItem />
-      <ChartsItem />
+      {/* <Login /> */}
+      {/* <p>hola</p> */}
+      {/* <TableItem /> */}
+      {/* <ChartsItem /> */}
     </div>
   );
 }
