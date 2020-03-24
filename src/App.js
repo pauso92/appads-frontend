678ad4228faeee6b49adec0e55efb001ddf42ed3
Edgar Tapia <edgar.tapia@outlook.es>
2020-03-23 19:33:04 -0600

    [edit] se actualizo la pagina de inicio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 
 import Report_view from './Report_view';
 import Login from './Login'
 //import logo from './logo.svg';
 import TableItem from './tableItem'
 import ChartsItem from './ChartsItem'
+import Inicio from './Registrate'
+import Registrate from './Registrate';
 //import './App.css'
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <ul>
-        <li>
-          <Link to="/Login">Login</Link>
-        </li>
-        <li>
+      <div className="wrap_router">
+        <Router className="router">
+          <ul className="ul_router">
+            <li className="li_router">
+              <Link className="link_router" to="/">Inico</Link>
+            </li>
+            <li>
+              <Link to="/Registro">Registrate</Link>
+            </li>
+            <li>
+              <Link to="/Login">Login</Link>
+            </li>
+            {/* <li>
           <Link to="/Report_view">Crear reporte</Link>
         </li>
         <li>
@@ -26,20 +35,23 @@ function App() {
         </li>
         <li>
           <Link to="/ChartsItem">ChartsItem</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route exact path="/Login" component={Login} />
-        <Route exact path="/Report_view" component={Report_view} />
-        <Route exact path="/TableItem" component={TableItem} />
-        <Route exact path="/ChartsItem" component={ChartsItem} />
-      </Switch>
-    </Router>
-      {/* < Report_view/> */}
-      {/* <Login /> */}
-      {/* <p>hola</p> */}
-      {/* <TableItem /> */}
-      {/* <ChartsItem /> */}
+        </li> */}
+          </ul>
+          <Switch>
+            {/* <Route exact path="/" component={Inicio} /> */}
+            <Route exact path="/Registro" component={Registrate} />
+            <Route exact path="/Login" component={Login} />
+            <Route exact path="/Report_view" component={Report_view} />
+            <Route exact path="/TableItem" component={TableItem} />
+            <Route exact path="/ChartsItem" component={ChartsItem} />
+          </Switch>
+        </Router>
+        {/* < Report_view/> */}
+        {/* <Login /> */}
+        {/* <p>hola</p> */}
+        {/* <TableItem /> */}
+        {/* <ChartsItem /> */}
+      </div>
     </div>
   );
 }
---
+++ src/Registrate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Registrate(){
+
+    return(
+        <>
+            <p>Aqui va el registro</p>
+        </>
+    )
+}
+
+export default Registrate
