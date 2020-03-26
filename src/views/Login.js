49fa064c61810e6270b01aaf84845aebadf039ce
MANUEL <vinim9395@gmail.com>
2020-03-26 01:57:01 -0600

    se suben cambios al login v.01

--- src/views/Login.js
+++ src/views/Login.js
@@ -6,13 +6,23 @@ class Login extends React.Component {
     render() {
         return (
             <>
+            <div className="navlogin">
+              <h1>AppAds</h1>
+               
+                <div className="alogin">
+                <a className="">Recursos</a>
+                <a className="">¿Quien soy?</a>
+                <a className="">Servicios</a>
+                <a className="">Login</a>
+                </div>
+                </div>
                 <div className="wrapp">
                     <div className="w_titulo">
-                        <p className="titulo">Inicia Sesión</p>
+                        <p className="titulo">Contactanos</p>
                     </div>
                     <div className="wrapp_user">
                         <p className="user">Usuario</p>
-                        <input className="input" placeholder="Ingresa usuario" type="name"></input>
+                        <input className="input" placeholder="Nombre" type="name"></input>
                         <p className="user">Password</p>
                         <input className="input" placeholder="Ingresa pasword" type="password"></input>
                     </div>
