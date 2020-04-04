f737d6a9ff14d5dd7436fa53d153d362bf2f7382
Yoel Ovando <yoeli.cruz20@gmail.com>
2020-04-03 21:04:51 -0600

    mio

--- src/components/ChartsItem.js
+++ src/components/ChartsItem.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Chart } from 'react-charts'
-import '../css/ChartsItem.css' 
+  import React from 'react'
+  import { Chart } from 'react-charts'
+  import '../css/ChartsItem.css' 
 
-const ReactChartComponent = () => {
-  const data = React.useMemo(
-    () => [
-      [[1, 10], [2, 15], [3, 10]],
-      [[1, 10], [2, 13], [3, 10]],
-      [[1, 14], [2, 10], [3, 13]]
-    ],
-    []
-  )
+  const ReactChartComponent = () => {
+    const data = React.useMemo(
+      () => [
+        [[1, 10], [2, 15], [3, 10]],
+        [[1, 10], [2, 13], [3, 10]],
+        [[1, 14], [2, 10], [3, 13]]
+      ],
+      []
+    )
 
-  const axes = React.useMemo(
-    () => [
-      { primary: true, type: 'linear', position: 'bottom' },
-      { type: 'linear', position: 'left' }
-    ],
-    []
+    const axes = React.useMemo(
+      () => [
+        { primary: true, type: 'linear', position: 'bottom' },
+        { type: 'linear', position: 'left' }
+      ],
+      []
   )
 
   return (
